d8a0a3546d744283a51f997ca7b1621f7df4939a
amirosein <amirhosein.abbasi594@gmail.com>
2019-02-22 22:29:51 +0330

    firt commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+  const system_input = (param1, param2) => {
+    console.log("input function")
+    };
+  const scenarios = (param1, param2) => {
+      console.log("scenarios")
+    };
+  const another_func = (param1, param2) => {
+        console.log("func3")
+    };
+
+  const APIs=["1","2"]
+  const 
+(function main() {
+  console.log("Welcome to WoT Tester")
+  system_input();
+
+   
+})();
