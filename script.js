ae20b88ccc55b7f58a1dc29eadd1715846ed1589
amirosein <amirhosein.abbasi594@gmail.com>
2019-03-10 12:55:15 +0330

    input structure changed

--- script.js
+++ script.js
@@ -5,17 +5,25 @@
     const fs = require('fs');
     let rawdata = fs.readFileSync('input.json');  
     let jsondata = JSON.parse(rawdata);  
-
     let things=jsondata.things;
     for(var i=0;i<things.length;i++){
-      console.log(things[i].services);
+      toAPI(things[i]);
+      console.log("********************")
     }
-  
-    console.log("********************")
-  
-    };
-  const scenarios = (param1, param2) => {
-      console.log("scenarios")
+  };
+  const toAPI = (thing) => {
+      console.log("we are going to find proper service calls of the thing named:", thing.name)
+      
+      var APIArr=[];
+      var IP=thing.IP;
+      var method=thing.method;
+      var services=thing.services;
+
+      for(var i in services){
+        console.log(services[i].path)
+      }
+    
+
     };
   const another_func = (param1, param2) => {
         console.log("func3")
